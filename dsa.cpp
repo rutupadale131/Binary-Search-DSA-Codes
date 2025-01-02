cd8e1a4ed1cd3c4ab37c0fdfe51793100328ea67
rutupadale131 <padalerutuja@gmail.com>
2025-01-02 14:14:34 +0530

    Correction

--- dsa.cpp
+++ dsa.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-   cout<<"Rutuja is the best developre on this planet.";
+   cout<<"Rutuja is the best developer on this planet.";
    
    cout<<"\n"<<"Juno is a very cute dog.";
    return 0;
